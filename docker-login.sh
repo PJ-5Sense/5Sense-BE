08540667779f13931cf4b79f33fdf74d5b44d2ec
taesik <taesikyoon97@naver.com>
2023-12-03 21:42:32 +0900

    chore

--- docker-login.sh
+++
@@ -1,9 +0,0 @@
-# docker login
-echo "dckr_pat_GbNbafixm5nIFcXMr816FH0jy3Y" | docker login --username taesik97 --password-stdin
-
-# docker build
-docker buildx build -t taesik97/oh-sense:tagname .
-
-# docker image push 
-docker image push taesik97/oh-sense:tagname
-
