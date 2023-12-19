dbba88b9445654fddbc60cce0523e227a4f7b1ca
taesik <taesikyoon97@naver.com>
2023-12-19 16:18:22 +0900

    AADMIN-188 cors 이슈 대응 코드 작성

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
   app.useGlobalPipes(
     new ValidationPipe({
