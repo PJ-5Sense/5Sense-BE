657361a791248c0b1f8840a5084bde1b4725b426
taesik <taesikyoon97@naver.com>
2024-03-13 21:52:01 +0900

    ADMIN-191 카테고리 목록 기획 변경에 따른 업데이트 응답값에 센터에서 사용하는 카테고리만 넘겨주기로 변경됨

--- src/lesson-category/category.service.ts
+++ src/lesson-category/category.service.ts
@@ -7,9 +7,16 @@ import { Repository } from 'typeorm';
 export class CategoryService {
   constructor(@InjectRepository(CategoryEntity) private readonly categoryRepository: Repository<CategoryEntity>) {}
 
-  async getCategories() {
-    return (await this.categoryRepository.find()).map(category => {
-      return { id: category.id, name: category.name, parentId: category.parentId };
-    });
+  async getCategories(centerId: number) {
+    // centerId가 지정된 카테고리와 centerId가 null인 카테고리를 함께 검색합니다.
+    return await this.categoryRepository
+      .createQueryBuilder('category')
+      .select(['category.id', 'category.name', 'category.parentId'])
+      .distinct()
+      .innerJoin('category.sessionCategories', 'session')
+      .innerJoin('category.durationCategories', 'duration')
+      .where('session.centerId = :centerId', { centerId })
+      .orWhere('category.centerId IS NULL', { centerId: null })
+      .getMany();
   }
 }
