67c8ae3dead735622a8477cafba4139a9e7d7a26
taesik <taesikyoon97@naver.com>
2024-04-01 16:31:34 +0900

    [AADMIN-253] 삭제하기 기능에 대한 논의가 필요함 - 보류

--- src/lesson-room/lesson-room.service.ts
+++ src/lesson-room/lesson-room.service.ts
@@ -21,4 +21,19 @@ export class LessonRoomService {
   async update(id: number, centerId: number, updateCenterRoomDTO: UpdateCenterRoomDTO) {
     return await this.lessonRoomRepository.update({ id, centerId }, { ...updateCenterRoomDTO });
   }
+
+  /**
+   * 강의실 삭제 조건
+   * 1. 수강중인 강의실이 없어야함
+   * 2. 종료된 클래스에서 사용했다면 해당 정보는 어떻게 해야하지?
+   *
+   * @param id
+   * @param centerId
+   * @returns
+   */
+  async delete(id: number, centerId: number) {
+    // TODO : 종료된 클래스에 정보가 있다면 어떻게 해야할지 논의하기
+
+    return await this.lessonRoomRepository.delete({ id, centerId });
+  }
 }
