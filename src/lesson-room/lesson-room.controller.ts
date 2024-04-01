948db654b43672d050833521a42052bced8ecab9
taesik <taesikyoon97@naver.com>
2024-04-01 15:25:46 +0900

    [AADMIN-252] 강의실 수정 기능 완료

--- src/lesson-room/lesson-room.controller.ts
+++ src/lesson-room/lesson-room.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put } from '@nestjs/common';
 import { LessonRoomService } from './lesson-room.service';
 import { CreateCenterRoomDTO } from './dto/create-room.dto';
 import { CurrentUser } from '../common/decorator/user.decorator';
+import { UpdateCenterRoomDTO } from './dto/update-room.dto';
 
 @Controller('lesson-rooms')
 export class LessonRoomController {
@@ -13,7 +14,21 @@ export class LessonRoomController {
 
     return {
       success: true,
-      message: `The lesson room has been successfully registered`,
+      message: 'The lesson room has been successfully created',
+    };
+  }
+
+  @Put('/:roomId')
+  async updateRoom(
+    @Body() updateCenterRoomDTO: UpdateCenterRoomDTO,
+    @Param('roomId') id: number,
+    @CurrentUser('centerId') centerId: number,
+  ) {
+    await this.lessonRoomService.update(id, centerId, updateCenterRoomDTO);
+
+    return {
+      success: true,
+      message: `The lesson room has been successfully updated`,
     };
   }
 }
--- src/lesson-room/lesson-room.service.ts
+++ src/lesson-room/lesson-room.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { LessonRoomEntity } from './entity/lesson-room.entity';
 import { Repository } from 'typeorm';
 import { CreateCenterRoomDTO } from './dto/create-room.dto';
+import { UpdateCenterRoomDTO } from './dto/update-room.dto';
 
 @Injectable()
 export class LessonRoomService {
@@ -15,5 +16,9 @@ export class LessonRoomService {
 
   async crete(createCenterRoomDTO: CreateCenterRoomDTO, centerId: number) {
     return await this.lessonRoomRepository.save({ ...createCenterRoomDTO, centerId });
+  }
+
+  async update(id: number, centerId: number, updateCenterRoomDTO: UpdateCenterRoomDTO) {
+    return await this.lessonRoomRepository.update({ id, centerId }, { ...updateCenterRoomDTO });
   }
 }
