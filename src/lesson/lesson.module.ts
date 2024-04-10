cd3903d8d4c03fe3e999c45e0218708b12498d85
taesik <taesikyoon97@naver.com>
2024-04-10 13:49:17 +0900

    [dev] 데이터베이스 오류 해결 - 회차반 등록 테이블 미등록된 부분이였음

--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -8,12 +8,14 @@ import { LessonCategoryModule } from 'src/lesson-category/category.module';
 import { LessonRepository } from './lesson.repository';
 import { LessonViewEntity } from './entity/lesson-view.entity';
 import { LessonScheduleModule } from '../lesson-schedule/lesson-schedule.module';
+import { LessonRegistrationModule } from 'src/lesson-registration/lesson-registration.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([DurationLessonEntity, SessionLessonEntity, LessonViewEntity]),
     LessonScheduleModule,
     LessonCategoryModule,
+    LessonRegistrationModule,
   ],
   controllers: [LessonController],
   providers: [LessonService, LessonRepository],
