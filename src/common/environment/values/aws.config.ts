a0281fb6806171a4f128f3b2f917112b37d4271c
taesik <taesikyoon97@naver.com>
2024-04-12 10:09:22 +0900

    console.log delete

--- src/common/environment/values/aws.config.ts
+++ src/common/environment/values/aws.config.ts
@@ -33,9 +33,7 @@ export default async () => {
   };
 
   if (process.env.NODE_ENV !== 'local') {
-    console.log('?');
     env = await getValue('aws');
-    console.log(env);
   }
 
   ValidateConfig(env, AwsConfig);
