62038988f5e7258959bc15f68f2f3b920fac35ef
taesik <taesikyoon97@naver.com>
2024-04-27 21:51:22 +0900

    [dev] swagger update - lesson update

--- src/feature-modules/session-lesson/dto/request/update-session-lesson.dto.ts
+++ src/feature-modules/session-lesson/dto/request/update-session-lesson.dto.ts
@@ -1,19 +1,24 @@
 import { IsInt, IsObject, Matches, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 import { LessonCategory } from '../../../combined-lesson/type/lesson-category.type';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateSessionLessonDTO {
+  @ApiProperty()
   @Matches(`^[a-zA-Z0-9가-힣ㄱ-ㅎㅏ-ㅣ\\s.,!?-]{1,20}$`)
   name: string;
 
   @Matches(`^[a-zA-Z0-9가-힣ㄱ-ㅎㅏ-ㅣ\\s.,!?-]{1,300}$`)
+  @ApiProperty()
   memo: string;
 
   @IsObject()
   @ValidateNested()
+  @ApiProperty({ type: LessonCategory })
   @Type(() => LessonCategory)
   category: LessonCategory;
 
   @IsInt()
+  @ApiProperty()
   teacherId: number;
 }
